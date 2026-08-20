21b173c6a2a38ade20c0f7619cf90de448b282c1
ElvisEnesi <jattoelvis00@gmail.com>
2026-08-20 17:08:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+// js file
+// side nav
+let show = document.querySelector(".show");
+let hide = document.querySelector('.hide');
+let nav = document.querySelector('.nav');
+// 
+show.addEventListener("click", function reveal() {
+    show.style.display = "none";
+    hide.style.display = "flex";
+    nav.style.right = "0";
+})
+hide.addEventListener("click", function close() {
+    show.style.display = "flex";
+    hide.style.display = "none";
+    nav.style.right = "-250px";
+})
+
+// slide show
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+
+// show password
+function showPassword() {
+    let x = document.getElementById("key");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+function revealPassword() {
+    let y = document.getElementsByClassName("key_show");
+    if (y.type === "password") {
+        y.type = "text";
+    } else {
+        y.type = "password";
+    }
+}
+
+// let openDash = document.querySelector("#openDash");
+// let closeDash = document.querySelector("#closeDash");
+// let aside = document.querySelector("#aside");
+
+// Open the sidebar
+function show_dash() {
+    let openDash = document.querySelector("#openDash");
+    let closeDash = document.querySelector("#closeDash");
+    let aside = document.querySelector("aside");
+    openDash.style.display = "none";
+    closeDash.style.display = "flex";
+    aside.style.left = "0px";
+};
+
+// Close the sidebar
+function hide_dash() {
+    let openDash = document.querySelector("#openDash");
+    let closeDash = document.querySelector("#closeDash");
+    let aside = document.querySelector("aside");
+    closeDash.style.display = "none";
+    openDash.style.display = "flex";
+    aside.style.left = "-270px"; // Change -250px to match your sidebar width
+};
